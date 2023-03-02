65a53c5866bedb7eaf0eeea50dc5c6959632129f
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-03-03 00:39:04 +0400

    salam

---
+++ array 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace array_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 5, 18, 98, 45, 3, 67 };
+            int max = a[0];
+            int min = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != max && a[i] < min)
+                {
+                    min = a[i];
+                }
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine($"{max}");
+            Console.WriteLine($"{min}");
+        }
+    }
+}
